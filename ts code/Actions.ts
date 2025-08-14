d0bbbddc51a699fc2d5e33967db15738dd9234e5
NoobLity <noobability21@gmail.com>
2025-08-14 12:31:17 -0400

    wdwa

--- ts code/Actions.ts
+++ ts code/Actions.ts
@@ -3,3 +3,17 @@ AOS.init({
   duration: 1000, // animation speed
   once: true      // only animate once
 });
+const boxes = document.querySelectorAll('.box');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); // stops re-triggering
+    }
+  });
+}, { threshold: 0.1 });
+
+boxes.forEach(box => {
+  observer.observe(box);
+});
