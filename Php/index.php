9b5ae424d3f491f3e12e5fa8754f5633c0870ebc
NoobLity <noobability21@gmail.com>
2025-09-08 20:01:13 -0400

    does not work

---
+++ Php/index.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "noobability21@gmail.com";
+    $subject = "Question about the Free Store";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
